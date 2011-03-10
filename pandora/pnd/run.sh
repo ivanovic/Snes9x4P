4db5236628a56bc3f74fe68765d442a1f5051ac2
Nils Kneuper <crazy-ivanovic@gmx.net>
2011-03-10 20:52:49 +0000

    switch to the latest picklelauncher

--- pandora/pnd/run.sh
+++ pandora/pnd/run.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-if [ ! -f "./profile.txt" ]; then
-    cp -R defconf/* ./
+
+cp --no-clobber -r defconf/* ./
+
+#old versions of snes9x came with a picklelauncher with different options
+#if such an option is found, the config file has to be replaced!
+if [ ! -z "$(grep "fontpath" ./config.txt)" ];
+then
+	cp defconf/config.txt ./config.txt
+	cp defconf/profile.txt ./profile.txt
 fi
+
 ./picklelauncher
