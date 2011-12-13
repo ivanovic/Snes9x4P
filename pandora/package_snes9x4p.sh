b02609054ed496a89d13363dcaa11fa4948fe535
Nils Kneuper <crazy-ivanovic@gmx.net>
2011-12-13 13:11:55 +0100

    fix packaging script to not rely on svn but use a plain copy

--- pandora/package_snes9x4p.sh
+++ pandora/package_snes9x4p.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 DATE=`date +%Y%m%d` # year month date
-svn export pnd pnd_tmp
+cp -r pnd pnd_tmp
 cp ../snes9x pnd_tmp/
 cp ../README.txt pnd_tmp/
 ./pnd_make.sh -p snes9x4p_$DATE.pnd -d pnd_tmp -x pnd_tmp/PXML.xml -i pnd_tmp/icon.png -c
